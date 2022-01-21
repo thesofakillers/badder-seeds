ae1162ba734f0494b3679b399ce35ad90f9d69c4
Giulio Starace <giulio.starace@gmail.com>
2022-01-21 11:01:52 +0100

    autoformat with black

--- badseeds/preprocess.py
+++ badseeds/preprocess.py
@@ -7,67 +7,77 @@ from tqdm import tqdm
 
 
 def process_sentence(sentence):
-	return nltk.tokenize.word_tokenize(sentence.lower())
+    return nltk.tokenize.word_tokenize(sentence.lower())
 
 
 try:
-	nltk.data.find("tokenizers/punkt")
+    nltk.data.find("tokenizers/punkt")
 except LookupError:
-	nltk.download("punkt")
+    nltk.download("punkt")
 
 if __name__ == "__main__":
-	if not os.path.isdir("../data/processed"):
-		os.makedirs("../data/processed")
-		print("Created folder : ", "../data/processed")
+    if not os.path.isdir("../data/processed"):
+        os.makedirs("../data/processed")
+        print("Created folder : ", "../data/processed")
 
-	if not os.path.isfile("../data/processed/nytimes_news_articles.pkl"):
-		with open("../data/nytimes_news_articles.txt", "r") as f:
-			all_sentences = []
-			for line in tqdm(f.readlines()):
-				if len(line) > 1 and line[:3] != "URL":
-					[all_sentences.append(process_sentence(i)) for i in line.split(".")]
-			all_sentences = [x for x in all_sentences if len(x) > 1]
-			pickle.dump(
-				all_sentences, open("../data/processed/nytimes_news_articles.pkl", "wb")
-			)
+    if not os.path.isfile("../data/processed/nytimes_news_articles.pkl"):
+        with open("../data/nytimes_news_articles.txt", "r") as f:
+            all_sentences = []
+            for line in tqdm(f.readlines()):
+                if len(line) > 1 and line[:3] != "URL":
+                    [all_sentences.append(process_sentence(i)) for i in line.split(".")]
+            all_sentences = [x for x in all_sentences if len(x) > 1]
+            pickle.dump(
+                all_sentences, open("../data/processed/nytimes_news_articles.pkl", "wb")
+            )
 
-	if not os.path.isfile("../data/processed/goodreads_reviews_history_biography.pkl"):
-		with open("../data/goodreads_reviews_history_biography.json", "r") as f:
-			all_sentences = []
-			for line in tqdm(f.readlines()):
-				data = json.loads(line)
-				text = data["review_text"]
-				text = [process_sentence(i) for i in text.split(".")]
-				[all_sentences.append(i) for i in text]
-			pickle.dump(
-				all_sentences, open("../data/processed/goodreads_reviews_history_biography.pkl", "wb")
-			)
+    if not os.path.isfile("../data/processed/goodreads_reviews_history_biography.pkl"):
+        with open("../data/goodreads_reviews_history_biography.json", "r") as f:
+            all_sentences = []
+            for line in tqdm(f.readlines()):
+                data = json.loads(line)
+                text = data["review_text"]
+                text = [process_sentence(i) for i in text.split(".")]
+                [all_sentences.append(i) for i in text]
+            pickle.dump(
+                all_sentences,
+                open("../data/processed/goodreads_reviews_history_biography.pkl", "wb"),
+            )
 
-	if not os.path.isfile("../data/processed/goodreads_reviews_romance.pkl"):
-		if not os.path.isdir('../data/processed/goodreads_reviews_romance'):
-			os.makedirs('../data/processed/goodreads_reviews_romance')
-		with open("../data/goodreads_reviews_romance.json", "r") as f:
-			all_sentences = []
-			for index, line in enumerate(tqdm(f.readlines())):
-				data = json.loads(line)
-				text = data["review_text"]
-				text = [process_sentence(i) for i in text.split(".")]
-				[all_sentences.append(i) for i in text]
+    if not os.path.isfile("../data/processed/goodreads_reviews_romance.pkl"):
+        if not os.path.isdir("../data/processed/goodreads_reviews_romance"):
+            os.makedirs("../data/processed/goodreads_reviews_romance")
+        with open("../data/goodreads_reviews_romance.json", "r") as f:
+            all_sentences = []
+            for index, line in enumerate(tqdm(f.readlines())):
+                data = json.loads(line)
+                text = data["review_text"]
+                text = [process_sentence(i) for i in text.split(".")]
+                [all_sentences.append(i) for i in text]
 
-				if index != 0 and index % 20000 == 0:
-					pickle.dump(
-						all_sentences, open(f"../data/processed/goodreads_reviews_romance/goodreads_reviews_romance_{index}.pkl", "wb")
-					)
-					all_sentences = []
+                if index != 0 and index % 20000 == 0:
+                    pickle.dump(
+                        all_sentences,
+                        open(
+                            f"../data/processed/goodreads_reviews_romance/goodreads_reviews_romance_{index}.pkl",
+                            "wb",
+                        ),
+                    )
+                    all_sentences = []
 
-	for file in os.listdir("../data/WikiText103/wikitext-103"):
-		filename = os.fsdecode(file)
-		if not os.path.isfile(f"../data/processed/{filename}.pkl"):
-			with open("../data/WikiText103/wikitext-103/" + filename, "r") as f:
-				all_sentences = []
-				for line in tqdm(f.readlines()):
-					if len(line) > 1 and line[:2] != " =":
-						[all_sentences.append(process_sentence(i)) for i in line.split(".")]
+    for file in os.listdir("../data/WikiText103/wikitext-103"):
+        filename = os.fsdecode(file)
+        if not os.path.isfile(f"../data/processed/{filename}.pkl"):
+            with open("../data/WikiText103/wikitext-103/" + filename, "r") as f:
+                all_sentences = []
+                for line in tqdm(f.readlines()):
+                    if len(line) > 1 and line[:2] != " =":
+                        [
+                            all_sentences.append(process_sentence(i))
+                            for i in line.split(".")
+                        ]
 
-				all_sentences = [x for x in all_sentences if len(x) > 1]
-				pickle.dump(all_sentences, open(f"../data/processed/{filename}.pkl", "wb"))
+                all_sentences = [x for x in all_sentences if len(x) > 1]
+                pickle.dump(
+                    all_sentences, open(f"../data/processed/{filename}.pkl", "wb")
+                )
