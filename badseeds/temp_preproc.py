82d6bdbe7781da0be5d8fa959f5f544a091a61f9
Jille <jillevandertogt@gmail.com>
2022-01-24 10:03:53 +0100

    new preprocessing

--- badseeds/temp_preproc.py
+++ badseeds/temp_preproc.py
@@ -5,6 +5,8 @@ import pickle
 import os
 import json
 import multiprocessing
+from spacy.tokens import DocBin
+import re
 
 # split a list into evenly sized chunks
 def chunks(l, n):
@@ -15,8 +17,24 @@ nlp = spacy.load(
     "en_core_web_sm", disable=["ner", "lemmatizer", "parser", "attribute_ruler"]
 )
 
+def save_file(documents, path):
+    doc_bin = DocBin(attrs=["TAG"])
 
-def preprocess_nyt(path_to_file, path_to_pickle):
+    for doc in tqdm(
+        nlp.pipe(
+            documents, disable=["ner", "lemmatizer", "parser", "attribute_ruler"]
+        ),
+        total=len(documents),
+    ):
+        doc_bin.add(doc)
+
+    bytes_data = doc_bin.to_bytes()
+
+    with open(path, "wb") as f:
+        f.write(bytes_data)
+
+
+def preprocess_nyt(name, save_path):
     """
     Tokenise, lowercase and POS tag NYT data, document-wise
     returns list of Spacy Docs
@@ -27,6 +45,8 @@ def preprocess_nyt(path_to_file, path_to_pickle):
     If you want to have more coarse POS tags, need to enable 'attribute_ruler'
     Resulting tags will be available from token.pos_
     """
+    path_to_file = "../data/" + name + '.txt'
+
     documents = []
     document = []
     with open(path_to_file, "r") as f:
@@ -47,12 +67,10 @@ def preprocess_nyt(path_to_file, path_to_pickle):
         else:
             continue
 
-    # save to pickle, its a very thicc file though, 4.5 GB, maybe not good
-    print("Done, saving to pickle")
-    with open(path_to_pickle, "wb") as f:
-        pickle.dump(documents, f)
+    print("Done. Now saving all documents in ", save_path)
+    save_file(documents, save_path)
 
-def preprocess_goodreads_romance(path_to_file, path_to_pickle):
+def preprocess_goodreads_romance(name, save_path):
     def do_job(job_id, data_slice):
         documents = []
         for index, line in enumerate(data_slice):
@@ -63,16 +81,18 @@ def preprocess_goodreads_romance(path_to_file, path_to_pickle):
             documents.append(document)
 
             if index != 0 and index % 15000 == 0 or index == len(data_slice)-1:
-                path_to_pickle = f'../data/processed/romance/goodreads_reviews_romance_{job_id}_{index}.pkl'
-                with open(path_to_pickle, 'wb') as f:
-                    pickle.dump(documents, f)
+                current_save_path = save_path + f'_{job_id}_{index}.bin'
+                save_file(documents, "".join(current_save_path))
                 documents = []
 
     if not os.path.isdir("../data/processed/romance"):
         os.makedirs('../data/processed/romance')
     
+    path_to_file = "../data/" + name + '.json'
     with open(path_to_file, 'r') as f:
         lines = f.readlines()
+    save_path = '../data/processed/romance/' + name
+
 
     total = len(lines)
     chunk_size = total // 4
@@ -85,18 +105,46 @@ def preprocess_goodreads_romance(path_to_file, path_to_pickle):
     for j in jobs:
         j.start()
 
-def create_documents(function, name):
-    path_to_file = "../data/" + name
-    name = name.split('.')[0]
-    path_to_pickle = '../data/processed/' + name + '.pkl'
+def preprocess_wiki(name, save_path):
+    path_to_file = "../data/WikiText103/wikitext-103/" + name
+    with open(path_to_file, "r") as f:
+        lines = f.readlines()
 
-    if not os.path.isfile(path_to_pickle):
-        print(f"Processing {name}")
-        function(path_to_file, path_to_pickle)
+    title_regex = re.compile(" = .* = \n")
+    subtitle_regex = re.compile(" = = .* = = \n")
+    new_line_regex = re.compile(" \n")
+
+    doc_idxs = [
+    idx
+    for idx, (prev_line, cur_line, next_line) in enumerate(
+        zip(lines[:-1], lines[1:], lines[2:]), 1
+    )
+    if new_line_regex.match(prev_line)
+    and new_line_regex.match(next_line)
+    and title_regex.match(cur_line)
+    and not subtitle_regex.match(cur_line)
+    ] + [len(lines) - 1]
+
+    documents = [
+    "".join(lines[start_idx:end_idx]).replace("\n", "").strip().lower()
+    for start_idx, end_idx in zip(doc_idxs, doc_idxs[1:])
+    ]
+
+    print("Done. Now saving all documents in ", save_path)
+    save_file(documents, save_path)
+
+def create_documents(function, name):
+    save_path = '../data/processed/' + name + '.bin'
+
+    if not os.path.isfile(save_path):
+        print(f"Processing all lines in {name}")
+        function(name, save_path)
     else:
         print(f"Skipping... File {name} already exists")
 
 
 if __name__ == "__main__":
-    create_documents(preprocess_nyt, "nytimes_news_articles.txt")
-    create_documents(preprocess_goodreads_romance, "goodreads_reviews_romance.json")
+    create_documents(preprocess_nyt, "nytimes_news_articles")
+    create_documents(preprocess_wiki, "wiki.train.tokens")
+    create_documents(preprocess_goodreads_romance, "goodreads_reviews_romance")
+
