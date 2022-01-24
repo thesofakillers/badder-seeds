903331dc922860f5e1999aefc8d278cdd4cbdfae
Jille <jillevandertogt@gmail.com>
2022-01-24 10:31:23 +0100

    autoformatted

--- badseeds/temp_preproc.py
+++ badseeds/temp_preproc.py
@@ -10,20 +10,20 @@ import re
 
 # split a list into evenly sized chunks
 def chunks(l, n):
-    return [l[i:i+n] for i in range(0, len(l), n)]
+    return [l[i : i + n] for i in range(0, len(l), n)]
+
 
 # tokenizer and tagger
 nlp = spacy.load(
     "en_core_web_sm", disable=["ner", "lemmatizer", "parser", "attribute_ruler"]
 )
 
+
 def save_file(documents, path):
     doc_bin = DocBin(attrs=["TAG"])
 
     for doc in tqdm(
-        nlp.pipe(
-            documents, disable=["ner", "lemmatizer", "parser", "attribute_ruler"]
-        ),
+        nlp.pipe(documents, disable=["ner", "lemmatizer", "parser", "attribute_ruler"]),
         total=len(documents),
     ):
         doc_bin.add(doc)
@@ -45,7 +45,7 @@ def preprocess_nyt(name, save_path):
     If you want to have more coarse POS tags, need to enable 'attribute_ruler'
     Resulting tags will be available from token.pos_
     """
-    path_to_file = "../data/" + name + '.txt'
+    path_to_file = "../data/" + name + ".txt"
 
     documents = []
     document = []
@@ -70,6 +70,7 @@ def preprocess_nyt(name, save_path):
     print("Done. Now saving all documents in ", save_path)
     save_file(documents, save_path)
 
+
 def preprocess_goodreads_romance(name, save_path):
     def do_job(job_id, data_slice):
         documents = []
@@ -80,19 +81,18 @@ def preprocess_goodreads_romance(name, save_path):
             document = nlp(document)
             documents.append(document)
 
-            if index != 0 and index % 15000 == 0 or index == len(data_slice)-1:
-                current_save_path = save_path + f'_{job_id}_{index}.bin'
+            if index != 0 and index % 15000 == 0 or index == len(data_slice) - 1:
+                current_save_path = save_path + f"_{job_id}_{index}.bin"
                 save_file(documents, "".join(current_save_path))
                 documents = []
 
     if not os.path.isdir("../data/processed/romance"):
-        os.makedirs('../data/processed/romance')
-    
-    path_to_file = "../data/" + name + '.json'
-    with open(path_to_file, 'r') as f:
-        lines = f.readlines()
-    save_path = '../data/processed/romance/' + name
+        os.makedirs("../data/processed/romance")
 
+    path_to_file = "../data/" + name + ".json"
+    with open(path_to_file, "r") as f:
+        lines = f.readlines()
+    save_path = "../data/processed/romance/" + name
 
     total = len(lines)
     chunk_size = total // 4
@@ -105,6 +105,7 @@ def preprocess_goodreads_romance(name, save_path):
     for j in jobs:
         j.start()
 
+
 def preprocess_wiki(name, save_path):
     path_to_file = "../data/WikiText103/wikitext-103/" + name
     with open(path_to_file, "r") as f:
@@ -115,26 +116,27 @@ def preprocess_wiki(name, save_path):
     new_line_regex = re.compile(" \n")
 
     doc_idxs = [
-    idx
-    for idx, (prev_line, cur_line, next_line) in enumerate(
-        zip(lines[:-1], lines[1:], lines[2:]), 1
-    )
-    if new_line_regex.match(prev_line)
-    and new_line_regex.match(next_line)
-    and title_regex.match(cur_line)
-    and not subtitle_regex.match(cur_line)
+        idx
+        for idx, (prev_line, cur_line, next_line) in enumerate(
+            zip(lines[:-1], lines[1:], lines[2:]), 1
+        )
+        if new_line_regex.match(prev_line)
+        and new_line_regex.match(next_line)
+        and title_regex.match(cur_line)
+        and not subtitle_regex.match(cur_line)
     ] + [len(lines) - 1]
 
     documents = [
-    "".join(lines[start_idx:end_idx]).replace("\n", "").strip().lower()
-    for start_idx, end_idx in zip(doc_idxs, doc_idxs[1:])
+        "".join(lines[start_idx:end_idx]).replace("\n", "").strip().lower()
+        for start_idx, end_idx in zip(doc_idxs, doc_idxs[1:])
     ]
 
     print("Done. Now saving all documents in ", save_path)
     save_file(documents, save_path)
 
+
 def create_documents(function, name):
-    save_path = '../data/processed/' + name + '.bin'
+    save_path = "../data/processed/" + name + ".bin"
 
     if not os.path.isfile(save_path):
         print(f"Processing all lines in {name}")
@@ -147,4 +149,3 @@ if __name__ == "__main__":
     create_documents(preprocess_nyt, "nytimes_news_articles")
     create_documents(preprocess_wiki, "wiki.train.tokens")
     create_documents(preprocess_goodreads_romance, "goodreads_reviews_romance")
-
