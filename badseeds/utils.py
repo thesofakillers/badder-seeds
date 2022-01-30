c2ea4c4f95dfcad3008bca80df35554730e85626
Giulio Starace <giulio.starace@gmail.com>
2022-01-30 18:31:08 +0100

    funcs for getting word embeddings with different query options

--- badseeds/utils.py
+++ badseeds/utils.py
@@ -1,10 +1,140 @@
 import numpy as np
-from preprocess import read_pproc_dataset
+from .preprocess import read_pproc_dataset
 import gensim.models as gm
 
 
+def get_embeddings(word_list, models, query_strat="average"):
+    """
+    Gets embeddings for a list of words
+
+    Parameters
+    -----------
+    word_list: array-like of strings
+        list of words to get embeddings for
+    models: list of gensim.models.KeyedVectors
+        list of KeyVectors objects mapping word:embedding
+        representing a set of models that may contain
+        embeddings for the words in word_list
+    query_strat: string, default "average"
+        strategy to use to get embeddings. Options are:
+        - "average": average the embeddings of all models that contain the word.
+        - "skip": skips word if one model does not contain it, otherwise average
+        - "first": take the embedding of the first model that contains the word.
+
+    Returns
+    -------
+    embeddings: array-like of floats
+        array of embeddings for the words in word_list
+        if 'skip', respective embedding for skipped words will be array-like of np.nan
+    """
+    if query_strat == "average":
+        embeddings = get_average_embeddings(word_list, models, allow_missing=True)
+    elif query_strat == "skip":
+        embeddings = get_average_embeddings(word_list, models, allow_missing=False)
+    elif query_strat == "first":
+        embeddings = get_first_embeddings(word_list, models)
+    else:
+        raise ValueError("query_strat must be one of 'average', 'skip', or 'first'")
+    return embeddings
+
+
+def get_first_embeddings(word_list, models):
+    """
+    Gets embeddings for a list of words from the first model that contains the word
+
+    Parameters
+    -----------
+    word_list: array-like of strings
+        list of W words to get embeddings for
+    models: list of gensim.models.KeyedVectors
+        list of KeyVectors objects mapping word:embedding
+        representing a set of M models that may contain
+        D-dimensional embeddings for the words in word_list
+
+    Returns
+    -------
+    embeddings: array-like of floats
+        (W, D) array of embeddings for the words in word_list
+        for words not found in any model, the embedding is set to an array of np.NaN
+    """
+    num_words = len(word_list)
+    # assuming embeddings in models are all the same dimensionality
+    emb_size = len(models[0][0])
+    embeddings = np.full((num_words, emb_size), np.nan)
+    for i, word in enumerate(word_list):
+        # in case word is not found in any model, set embedding to NaN
+        embedding = np.full(emb_size, np.nan)
+        for model in models:
+            if word in model:
+                embedding = model[word]
+                # exit loop early if word found
+                break
+        embeddings[i] = embedding
+    return embeddings
+
+
+def get_average_embeddings(word_list, models, allow_missing=True):
+    """
+    Gets average embeddings for a list of words across a set of models
+
+    Parameters
+    -----------
+    word_list: array-like of strings
+        list of W words to get embeddings for
+    models: list of gensim.models.KeyedVectors
+        list of KeyedVectors
+        representing a set of M models that may contain
+        D-dimensional embeddings for the words in word_list
+    allow_missing: boolean, default True
+        if True, when embeddings are missing in a model,
+        the average is taken over all models that contain the word.
+        if False, the embedding is set to an array of np.NaN for the missing word.
+
+
+    Returns
+    -------
+    embeddings: array-like of floats
+        (W, D) array of embeddings for the words in word_list
+        The embedding is set to an array of np.NaN when none
+        of the models contain the word, regardless of allow_missing
+    """
+    num_models = len(models)
+    num_words = len(word_list)
+    # assuming embeddings in models are all the same dimensionality
+    emb_size = len(models[0][0])
+
+    embeddings = np.full((num_words, emb_size), np.nan)
+    # get (M, D) embedding for word
+    for w, word in enumerate(word_list):
+        # first initialize with NaNs
+        embedding = np.full((num_models, emb_size), np.nan)
+        # fill in embeddings for models that contain the word
+        for i, model in enumerate(models):
+            if word in model:
+                embedding[i, :] = model[word]
+        # can now get (D, ) average embedding
+        if allow_missing:
+            # if word not found anywhere, just return array of nans
+            if np.isnan(embedding).all():
+                embedding = np.full(emb_size, np.nan)
+            # average embedding, allow for some nans
+            else:
+                embedding = np.nanmean(embedding, axis=0)
+        else:
+            # average embeddings, not allowing for nans
+            embedding = np.mean(embedding, axis=0)
+            # if any nans in result, set entire embedding to NaN
+            if np.isnan(embedding).any():
+                embedding = np.full((emb_size), np.nan)
+
+        # set embedding for word
+        embeddings[w] = embedding
+
+    return embeddings
+
+
 def bootstrap(dataset, n=20):
-    """ "
+    """
     makes bootstrap samples from given dataset
 
     Parameters
