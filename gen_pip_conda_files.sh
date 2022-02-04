c3b273737d8b61cfed9a740657b186fed035f5e4
Giulio Starace <giulio.starace@gmail.com>
2022-02-04 22:03:32 +0100

    details about PATH

--- gen_pip_conda_files.sh
+++ gen_pip_conda_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "generating conda env file"
-conda env export > environment.yml
+conda env export --no-builds > environment.yml
 echo "generating pip reqauirements file"
 poetry export --without-hashes -o requirements.txt
